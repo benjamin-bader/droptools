baf72ac4ac1f599e96d795f53accc5198359c669
Ben Bader <ben@getkeepsafe.com>
2014-10-03 11:49:50 -0700

    Creating initial gh-pages

--- src/main/java/com/bendb/dropwizard/jooq/JooqConfiguration.java
+++
@@ -1,8 +0,0 @@
-package com.bendb.dropwizard.jooq;
-
-import io.dropwizard.Configuration;
-import io.dropwizard.db.DatabaseConfiguration;
-
-public interface JooqConfiguration<C extends Configuration> extends DatabaseConfiguration<C> {
-    JooqFactory getJooqFactory(C configuration);
-}
