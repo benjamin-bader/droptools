03e149501d729b0bc1dcfd57521ca53ddeddc45e
Ben Bader <ben@bendb.com>
2020-07-09 22:18:10 -0700

    Strip 'docs/' from links in README.md when publishing mkdocs site

--- deploy_site.sh
+++ deploy_site.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cat README.md | grep -v "Build Status" | grep -v "Coverage Status" > docs/index.md
+cat README.md | grep -v "Build Status" | grep -v "Coverage Status" | sed 's_docs/__' > docs/index.md
 
 mkdocs gh-deploy
 
